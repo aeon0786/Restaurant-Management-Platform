cf484d39f4cb88334436d260450f22c0b1a244b4
Afshin <afshinthn@gmail.com>
2026-06-03 16:51:08 +0330

    Customer added

---
+++ Customer.cpp
@@ -0,0 +1,42 @@
+#include "Customer.h"
+
+Customer::Customer (string name, string pass) 
+    : User(name, pass),
+    currentOrder(nullptr),
+    balance(0)
+{}
+void Customer::DisplayOrderHistory () const
+{
+    for (const auto &order : orders)
+    {
+        cout << "~~~~~~~~~~~  " << order.getName() << "  ~~~~~~~~~~~" << endl;
+        order.DispalyList();
+    }
+}
+void Customer::addBalance (double amount) { balance += amount; }
+double Customer::getBalance () const { return balance; }
+
+void Customer::createNewOrder (string name) 
+{
+    if (currentOrder) delete currentOrder;
+    currentOrder = new Order(name);
+}
+bool Customer::finalizeOrder ()
+{
+    if (!currentOrder)
+        cout << "There are no orders in the cart." << endl;
+        return false;
+    
+    double total = currentOrder->calculateTotalPrice();
+    if (balance >= total)
+    {
+        balance -= total;
+        currentOrder = nullptr;
+        return true;
+    }
+    else 
+    {
+        cout << "There were not enough wallet balances." << endl;
+        return false;
+    }
+}
---
+++ Customer.h
@@ -0,0 +1,19 @@
+#pragma once 
+#include <vector>
+#include "History_Of_Orders.h"
+#include "User.h"
+
+class Customer:public User
+{
+    private:
+        vector<List> orders;
+        Order* currentOrder;
+        double balance;
+    public:
+        Customer (string, string);
+        void addBalance (double);
+        double getBalance () const;
+        void createNewOrder (string);
+        bool finalizeOrder ();
+        void DisplayOrderHistory () const;
+};
